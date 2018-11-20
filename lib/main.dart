23fa8c0cfcfaae3c64154ca6bd9be721c979238a
HEL Mab <mabhelitc@gmail.com>
2018-11-20 22:05:19 +0700

    add drawer and tab bar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './product_manager.dart';
+import './pages/about.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,15 +14,70 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [];
+
+  int onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text("App Name"),
+              ),
+              ListTile(
+                title: Text("Home"),
+                onTap: () {
+                  print("Clicked on Tap");
+                },
+              ),
+              ListTile(
+                title: Text("About"),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => AboutPage()));
+                },
+              ),
+              ListTile(
+                title: Text("Settings"),
+                onTap: () {
+                  print("Clicked on Tap");
+                },
+              )
+            ],
+          ),
+        ),
         appBar: AppBar(
           title: Text("Khmer Unit"),
         ),
         body: ProductManager(),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.home),
+              title: new Text('Home'),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.mail),
+              title: new Text('Messages'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('Profile'),
+            ),
+          ],
+        ),
       ),
     );
   }
