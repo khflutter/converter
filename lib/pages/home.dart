afbe2d291f23b84d3fac78496aa3433be13e5c61
HEL Mab <mabhelitc@gmail.com>
2018-12-16 10:54:10 +0700

    add lang

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:khunit/lang/DemoLocalizations.dart';
+import 'package:khunit/lang/DemoLocalizationsDelegate.dart';
 import 'package:khunit/widgets/drawer.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -13,6 +16,22 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
+      supportedLocales: [const Locale('en', 'US'), const Locale('tr', 'TR')],
+      localizationsDelegates: [
+        const DemoLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      localeResolutionCallback: (Locale locale, Iterable<Locale> supportedLocales) {
+        for (Locale supportedLocale in supportedLocales) {
+          if (supportedLocale.languageCode == locale.languageCode ||
+              supportedLocale.countryCode == locale.countryCode) {
+            return supportedLocale;
+          }
+        }
+
+        return supportedLocales.first;
+      },
       theme: ThemeData(
         primaryColor: Color(0xFF484B5B),
       ),
@@ -30,9 +49,7 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Center(
             child: Text(
               appName,
-              style: TextStyle(
-                fontFamily: "khmerM1"
-              ),
+              style: TextStyle(fontFamily: "khmerM1"),
             ),
           ),
         ),
